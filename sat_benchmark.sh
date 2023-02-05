2d45986cb444a1a7d13431b92457c2db9ae1dd16
Phuong Nguyen <philipsnguyen49@gmail.com>
2023-02-05 16:22:52 +0100

    update sat_benchmark script

--- sat_benchmark.sh
+++ sat_benchmark.sh
@@ -41,6 +41,10 @@ lbdm="DEFAULT"
 
 #Resharing
 cfci=500
+#number of conflict clause to switch to a new cfci
+nccts=100000
+#switched cfici
+scfci=2
 
 # 8 for normal utilization, keeping hardware threads idle
 # 4 for full utilization, spawning a solver at each hardware thread
@@ -77,7 +81,7 @@ baselogdir="myexperiment"
 sublogdir="${baselogdir}/${portfolio}-cbdf${cbdf}-T${timeout}"
 
 # TODO Add further options to these arguments Mallob is called with.
-malloboptions="-t=4 -T=$timeout -v=3 -sleep=1000 -appmode=fork -v=3 -interface-fs=0 -trace-dir=. -pipe-large-solutions=0 -processes-per-host=$NPROCS -regular-process-allocation -max-lits-per-thread=50000000 -strict-clause-length-limit=20 -clause-filter-clear-interval=$cfci -max-lbd-partition-size=2 -export-chunks=20 -clause-buffer-discount=$cbdf -satsolver=$portfolio -ccb=$ccb -spo=$spo -lbdm=$lbdm"
+malloboptions="-t=4 -T=$timeout -v=3 -sleep=1000 -appmode=fork -v=3 -interface-fs=0 -trace-dir=. -pipe-large-solutions=0 -processes-per-host=$NPROCS -regular-process-allocation -max-lits-per-thread=50000000 -strict-clause-length-limit=20 -clause-filter-clear-interval=$cfci -nccts=$nccts -scfci=$scfci -max-lbd-partition-size=2 -export-chunks=20 -clause-buffer-discount=$cbdf -satsolver=$portfolio -ccb=$ccb -spo=$spo -lbdm=$lbdm"
 
 #####################################################################
 
